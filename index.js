499614f140a88165d42402104af214e176a42200
jmaldama8110 <josemanuek@hotmail.com>
2021-06-09 16:47:10 -0500

    re-engineering of the original code

--- index.js
+++ index.js
@@ -1,75 +1,16 @@
 
-/*  
-    displayState = 100 -> default state, ready to set firstValue until the operator is assinged
-    displayState = 200 -> 
-    displayState = 300 -> 
- */
-let displayState = 100;
-let index = 0;
-
-let firstValue = '';
-let secondValue = '';
-let operator = '';
-
-const calcRecord = [];
-
-window.addEventListener('keydown', function (e) {
-
-    const display = document.querySelector('#displaytxt');
-
-    if (displayState == 100) {
-
-        const keyNumber = parseInt(e.key);
-        if ((keyNumber >= 0) && (keyNumber <= 9)) { /// here we are validating the key entry as an Number
-            
-            const cur = display.textContent === '0' ? '' : display.textContent;
-            display.textContent = `${cur}${e.key}`; // allows to enter any figure
-            
-            firstValue = display.textContent; // 100 state and pressing the operator means the user has finished entering the firstValue
-        }
-
-        if ( isAnOperator(e.key) && !operator) {
-
-            operator = e.key; // now the operator is assign
-            displayState = 200; // set the next state
-        }
-
-    }
-
-    if (displayState == 200) { 
-
-        
-        const keyNumber = parseInt(e.key);
-        if ((keyNumber >= 0) && (keyNumber <= 9)) { /// here we are validating the key entry as an Number
-            
-            if( !secondValue ) /// clears the display for the second value
-                display.textContent = '';
-                
-            const cur = display.textContent === '0' ? '' : display.textContent;
-            display.textContent = `${cur}${e.key}`; // allows to enter any figure
-            secondValue = display.textContent;
-        }
-
-        if( e.key === 'Enter' ){ /// when Equal is pressed (Enter ), then has to execute the appropitate operation
-
-            const result = operate(operator,firstValue,secondValue);
-            display.textContent = result.toString();
-
-            displayState = 300;
-
-        }
-
-    }
-
-    if( displayState == 300 ){
-        console.log(firstValue,operator,secondValue);
-    }
+let firstNumber = 0, secondNumber = 0, result = 0, operator = '';
+let display = '';
 
 
+window.addEventListener('keypress', function (e) {
+    if (e.key == 'Enter')
+        processEntryKey('=');
+    processEntryKey(e.key);
 });
 
 
-
+/// For buttons with Numbers, adds the eventListener
 const buttons = document.querySelectorAll('.item');
 buttons.forEach((item) => {
 
@@ -86,53 +27,137 @@ buttons.forEach((item) => {
     item.addEventListener('click', function () {
 
         const digit = this.textContent;
-        const display = document.querySelector('#displaytxt');
-        const cur = display.textContent === '0' ? '' : display.textContent;
-        display.textContent = `${cur}${digit}`;
+
+        if (digit == 'AC'){
+            clearEntries();
+            setDisplay('0');
+        } // when the Clear buttons is pressed
+        else
+            processEntryKey(digit);
 
     });
 });
 
+function processEntryKey(eKey) {
 
-function clearDisplay() {
-    const display = document.querySelector('#displaytxt');
-    display.textContent = '0';
+    const entry = entryType(eKey);
 
-    firstValue = '';
-    secondValue = '';
+    if (!firstNumber) { // as many numbers and one DOT as the firstValue is no assigned
+        if (entry.type == 'number' ||
+            (entry.type == 'dot' && !hasDot(display))) {
+            display = display + eKey;
+            setDisplay(display);
+        }
+
+        if (entry.type == 'operator') {
+
+            if (!entry.singleOperator) { // when the operator is a two number operator
+                firstNumber = parseFloat(display);
+                operator = entry.operator;
+            }
+        }
+    } else {
+        if (!secondNumber){
+            display = '';
+            setDisplay(display);
+        }
+
+        if (entry.type == 'number' ||
+            (entry.type == 'dot' && !hasDot(display))) {
+            display = display + eKey;
+            secondNumber = parseFloat(display);
+            setDisplay(display);
+
+        }
+
+        if (entry.type == 'equal') {
+            result = operate(operator, firstNumber, secondNumber);
+            display = parseFloat(result);
+            setDisplay(display);
+        }
+
+    }
+
+}
+
+
+const hasDot = (str) =>
+    str.indexOf('.') !== -1;
+
+const clearEntries = () => {
+    firstNumber = 0;
     operator = '';
-    displayState = 100;
+    secondNumber = 0;
+    result = 0;
+    display = '';
 }
 
-const btnClear = document.querySelector('#btnAC');
-btnClear.addEventListener('click', clearDisplay);
+const entryType = (key) => {
 
+    const isNumber = parseInt(key);
 
-function isAnOperator(key) {
-    return (key === '+' || key === '-' || key === '*' || key === '/' || key === '%' || key === 'Enter');
+    if ((isNumber >= 0) && (isNumber <= 9)) {
+        return {
+            type: 'number',
+            operator: '',
+            singleOperator: false
+        }
+    }
+
+    if (key === '+' || key === '-' || key === '*' || key === '/' || key === '%' || key === '!') {
+
+        return {
+            type: 'operator',
+            operator: key,
+            singleOperator: (key === '!' || key === '%') ? true : false
+        }
+    }
+
+    if (key === '=') {
+        return {
+            type: 'equal',
+            operator: key,
+            singleOperator: false
+        }
+    }
+    if (key === '.') {
+        return {
+            type: 'dot',
+            operator: key,
+            singleOperator: false
+        }
+    }
 
 }
 
-const add = (a,b)=> parseInt(a) + parseInt(b);
-const substract = (a,b)=> parseInt(a) - parseInt(b);
-const multiply = (a,b) => parseInt(a) * parseInt(b);
-const divide = (a,b) => parseInt(a)/parseInt(b);
-const percentage = (a) => parseInt(a)/100;
-const shiftSign = (a) => (parseInt(a)*(-1));
+function setDisplay(txt) {
+    const display = document.querySelector('#displaytxt');
+    display.textContent = txt;
+
+}
 
 
-function operate (opt, a, b ) {
 
-    switch( opt ){
+const add = (a, b) => (a) + (b);
+const substract = (a, b) => (a) - (b);
+const multiply = (a, b) => (a) * (b);
+const divide = (a, b) => (a) / (b);
+const percentage = (a) => (a) / 100;
+const shiftSign = (a) => ((a) * (-1));
+
+
+function operate(opt, a, b) {
+
+    switch (opt) {
         case '+':
-            
-            return add(a,b);
+
+            return add(a, b);
         case '-':
-            return substract(a,b);
+            return substract(a, b);
         case '*':
-            return multiply(a,b);
+            return multiply(a, b);
         case '/':
-            return divide(a,b);
+            return divide(a, b);
         case '%':
             return percentage(a);
         case '!':
